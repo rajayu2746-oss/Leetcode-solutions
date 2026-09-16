6f5f67d16b9b291a44d381d14182dfc95ff23be8
rajayu2746-oss <rajayu2746@gmail.com>
2026-09-16 18:24:10 +0530

    Time: 29 ms (94.57%) | Memory: 48.7 MB (32.35%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet < Integer > set = new HashSet<>();
+        for(int c : candyType){
+            set.add(c);
+        } return Math.min(set.size() , candyType.length / 2);
+    }
+}
